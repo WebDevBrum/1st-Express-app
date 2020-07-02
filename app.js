84ebae7a9ac03b190527ebecfe7bfb5c0f55e03c
Scott Davies <scottdavies1983@gmail.com>
2020-07-02 22:38:56 +0100

    end of session

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
-app.set("view engine", "pug");
+app.use(bodyParser.urlencoded({ extended: false }));
+app;
 
+app.set("view engine", "pug");
+app.use(cookieParser());
 app.get("/", (req, res) => {
   res.render("index");
 });
@@ -12,6 +17,16 @@ app.get("/cards", (req, res) => {
   res.render("card", {
     prompt: "Who is buried in Grants tomb?",
   });
+});
+
+app.get("/hello", (req, res) => {
+  res.render("hello", { name: req.cookies.username });
+});
+
+app.post("/hello", (req, res) => {
+  //console.log(req.body);
+  res.cookie("username", req.body.username);
+  res.render("hello", { name: req.body.username });
 });
 
 app.listen(3000, () => {
