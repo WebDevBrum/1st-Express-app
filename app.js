55d2e521c33ae0721452da72da89a388776ac684
Scott Davies <scottdavies1983@gmail.com>
2020-07-09 23:10:31 +0100

    removed node modules

--- app.js
+++ app.js
@@ -19,25 +19,25 @@ const cardRoutes = require("./routes/cards");
 app.use(mainRoutes); // calls the routes module
 app.use("/cards", cardRoutes); //used for flashcard routes, using cardroutes variable
 
-app.use((req, res, next) => {
-  //req.message = "This message made it"; //.message could be any name
-  console.log("hello");
-  //const err = new Error("on no!");
-  //err.status = 500;
-  next(); //err also taken out of next
-});
+//app.use((req, res, next) => {
+//req.message = "This message made it"; //.message could be any name
+//console.log("hello");
+//const err = new Error("on no!");
+//err.status = 500;
+// next(); //err also taken out of next
+////});
 
-app.use((req, res, next) => {
-  //console.log(req.message); //is passed message from above
-  console.log("world");
-  next();
-});
+//app.use((req, res, next) => {
+//console.log(req.message); //is passed message from above
+// console.log("world");
+// next();
+//});
 
-app.use((req, res, next) => {
-  const err = new Error("Not Found");
-  err.status = 404;
-  next(err);
-});
+//app.use((req, res, next) => {
+//const err = new Error("Not Found");
+//err.status = 404;
+//next(err);
+//////});
 
 //app.use((err, req, res, next) => {
 //induced error
